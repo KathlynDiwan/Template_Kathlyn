4719eabfd3740dfa0ddaa6f97f796ca92bb1da45
kathlyn diwan <kathlyn.diwan@sou.inteli.edu.br>
2022-06-02 20:53:25 -0300

    mudanças no exercício de lançamento vertical

--- Entregáveis M2/SEMANA 3/script.js
+++ Entregáveis M2/SEMANA 3/script.js
@@ -3,14 +3,20 @@
 function  ex1() {        //criar uma função
 
 //criando uma mensagem de aviso em alert 
-alert('Kathy você é top');
+alert('Você vai descobrir os valores');
 
 var v0 = document.getElementById ('v0').value; //representa a velocidade inicial - o get é o valor que iremos inserir na via web 
 const g = 10; //aceleração gravitacional 10m/sˆ2
 
 var //adicionar nova varrável que vai indicar o cálculo 
 
-tSubida = v0/g; //tempo de subida
+aAltura = v0 * v0/2 * g;  //função que foi dada que representa a altura máxima --> tive um pouco de dificuldade para representar o v0ˆ2/2g
+document.getElementById('vem').innerHTML = "ALTURA MÁXIMA = " +  aAltura; 
+console.log ("ALTURA MÁXIMA = " + aAltura)
+alert ("ALTURA MÁXIMA = " + aAltura); 
+
+
+tSubida = v0/g; //função que foi dada que representa o tempo de subida
 document.getElementById('vem').innerHTML =  "TEMPO DE SUBIDA = " + tSubida; //propriedade de 'colocar no html' 
 console.log ("TEMPO DE SUBIDA = " + tSubida); 
 alert ("TEMPO DE SUBIDA = " + tSubida); 
