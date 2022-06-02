e41d3c7494f9cbabadce654f2965b9a01da47c6e
KathlynDiwan <99209712+KathlynDiwan@users.noreply.github.com>
2022-06-02 09:32:32 -0300

    Add files via upload

---
+++ LANÇAMENTO VERTICAL/script.js
@@ -0,0 +1,18 @@
+//comentando em js 
+
+function  ex1() {        //criar uma função
+
+//criando uma mensagem de aviso em alert 
+alert('Kathy você é top');
+
+var v0 = document.getElementById ('v0').value; //representa a velocidade inicial - o get é o valor que iremos inserir na via web 
+const g = 10; //aceleração gravitacional 10m/sˆ2
+
+var //adicionar nova varrável que vai indicar o cálculo 
+
+tSubida = v0/g; //tempo de subida
+document.getElementById('vem').innerHTML =  "TEMPO DE SUBIDA = " + tSubida; //propriedade de 'colocar no html' 
+console.log ("TEMPO DE SUBIDA = " + tSubida); 
+alert ("TEMPO DE SUBIDA = " + tSubida); 
+
+}
